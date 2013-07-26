d364f0b8cf7dddc283d0d6bb23cce6cd0bd8351f
smith11235 <smith11235@gmail.com>
2013-07-26 17:13:36 -0400

    initial rails app setup, with gemspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resume::Application.config.secret_token = '47c3f005dc54e2e3ca9459d61b4ca094504ff9d372240f1312d065b8b5512d67e197462446c3a9860cae048d2d2d94cd476f7a07fbb822709c9b7d98250cda48'
